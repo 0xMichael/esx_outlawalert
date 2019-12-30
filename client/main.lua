853e9f5f574dee82bc82f0e0e650b6440e4cfcfa
FlawwsX <55849405+FlawwsX@users.noreply.github.com>
2019-12-30 22:48:15 +0000

    Adds chat and mythic notify support

--- client/main.lua
+++ client/main.lua
@@ -32,7 +32,14 @@ end)
 RegisterNetEvent('esx_outlawalert:outlawNotify')
 AddEventHandler('esx_outlawalert:outlawNotify', function(alert)
 	if isPlayerWhitelisted then
-		ESX.ShowNotification(alert)
+		--ESX.ShowNotification(alert)
+		exports['mythic_notify']:SendAlert('error', alert, 10000)
+
+		TriggerEvent('chat:addMessage', {
+			template = '<div style="padding: 0.5vw; margin: 0.5vw; background-color: rgba(255, 0, 0, 0.616); border-radius: 10px;">{0}</div>',
+			--template = '<div class="chat-message"><div class="chat-message-header">[OOC] {0}:</div><div class="chat-message-body">{1}</div></div>',
+			args = { alert }
+		})
 	end
 end)
 
